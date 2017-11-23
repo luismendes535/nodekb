33f1bb35a834f08e6246fa6b0c9c54c0be723f52
Luis Mendes <luismendes535@gmail.com>
2017-11-23 11:58:15 +0000

    Tutorial completed
    ...

--- app.js
+++ app.js
@@ -5,13 +5,14 @@ const bodyParser = require('body-parser');
 const expressValidator = require('express-validator');
 const flash = require('connect-flash');
 const session = require('express-session');
-
+const passport = require('passport');
+const config = require('./config/database')
 const dbConnectionOptions = {
     useMongoClient: true,
     authSource: "admin"
 }
 
-mongoose.connect('mongodb://localhost/nodekb', dbConnectionOptions);
+mongoose.connect(config.database, dbConnectionOptions);
 //mongoose.Promise = global.Promise;
 /*
 var Cat = mongoose.model('Cat', { name: String });
@@ -91,6 +92,17 @@ app.use(expressValidator({
   }
 }));
 
+//Passport Config
+require('./config/passport')(passport);
+//Passport Middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.get('*', function(req, res, next){
+  res.locals.user = req.user || null;
+  next();
+})
+
 //home route
 app.get('/', function(req, res){
   Article.find({}, function(err, articles){
@@ -112,6 +124,6 @@ app.use('/articles', articles);
 app.use('/users', users);
 
 //Start Server
-app.listen(3004, function(){
-  console.log('Server started in port 3004....')
+app.listen(3009, function(){
+  console.log('Server started in port 3009...')
 });
